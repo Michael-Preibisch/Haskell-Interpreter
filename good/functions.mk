77cc1ad98e979bf705b59622d319882add9559c9
Michal <michal.preibisch@gmail.com>
2017-05-05 18:29:48 +0200

    Some grammar files

---
+++ good/functions.mk
@@ -0,0 +1,11 @@
+int main () {
+
+  // Wypisze silnie pierwszych 10 liczb całkowitych
+  for i = 1 to (9 + 1) do {
+  	if (i <= 5)
+      printInt(fact(i)) ;
+    else
+      printInt(factr(i)) ;
+  }
+  return 0 ;
+}
